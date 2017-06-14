c4c88560c5867f55a7e22cdc281a50bf9be2e1b4
Udo <writeudo@gmail.com>
2017-06-15 00:24:57 +0200

    fix regex pattern

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
 'use strict';
 
-
+var naijaPhoneNumber = {};
 
 /**
- * Expose `naijaPhoneNumber`.
+ * Validate phone number
+ * @param  {string}  n
+ * @return {Boolean} 
  */
-
-module.exports = naijaPhoneNumber['default'] = naijaPhoneNumber;
-
 naijaPhoneNumber.isValid = function isValid(n) {
 
-  var isValid = false;
+  var firstChar;
+  var number;
+  var pattern = /^([0]{1})([7-9]{1})([0|1]{1})([1-9]{1})([\d]{7,8})$/g;
 
-  var reg = /^0[7-9][01][1-9][\d]{7,8}$/;
+  if (!n || n.length < 5) return false;
 
-  if (n && n.length > 5) {
+  if (typeof n === 'number') {
 
-    var firstChar = n.charAt(0);
-    var number = firstChar === 0 ? n : 0 + 'n';
+    number = '0' + n;
+
+  } else if (typeof n === 'string') {
+
+    firstChar = n.substring(0, 1);
+
+    number = (firstChar === '0') ? n : '0' + n;
+
+  } else {
+
+    return false;
 
   }
 
-  return isValid;
+  return pattern.test(number);
 
 };
+
+module.exports = naijaPhoneNumber;
