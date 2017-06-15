76b276dddb10890ca9ac24f7995080e70d899bc9
Udo <writeudo@gmail.com>
2017-06-15 21:35:30 +0200

    test all mobile networks

--- test.js
+++ test.js
@@ -3,17 +3,95 @@
 var assert = require('assert');
 var naijaNumber = require('./index.js');
 
+let prefixes = {
+  MTN: ['0803', '0806', '0703', '0706', '0813', '0816', '0810', '0814', '0903'],
+  Airtel: ['0708', '0802', '0808', '0812', '0701', '0902'],
+  GLO: ['0705', '0815', '0805', '0807', '0811', '0905'],
+  Etisalat: ['0809', '0817', '0818', '0909', '0908'],
+  Starcomms: ['07028', '07029', '0819'],
+  Visafone: ['0704', '07025', '07026'],
+  Multilinks: ['0709', '07027'],
+  Zoom: ['0707'],
+  MTEL: ['0804']
+};
+
+let suffix = '9764320';
+
 describe('Test Numbers', function() {
 
   describe('Network providers', function() {
     it('should return false for unknown network providers', function() {
       assert.equal(false, naijaNumber.isValid('05018154929'));
     });
+
+    it('MTN numbers', function() {
+      let result = prefixes.MTN.map(function(prefix) {
+        return naijaNumber.isValid(prefix + suffix);
+      });
+      assert.equal(-1, result.indexOf(false));
+    });
+
+    it('GLO numbers', function() {
+      let result = prefixes.GLO.map(function(prefix) {
+        return naijaNumber.isValid(prefix + suffix);
+      });
+      assert.equal(-1, result.indexOf(false));
+    });
+
+    it('Airtel numbers', function() {
+      let result = prefixes.Airtel.map(function(prefix) {
+        return naijaNumber.isValid(prefix + suffix);
+      });
+      assert.equal(-1, result.indexOf(false));
+    });
+
+    it('Etisalat numbers', function() {
+      let result = prefixes.Etisalat.map(function(prefix) {
+        return naijaNumber.isValid(prefix + suffix);
+      });
+      assert.equal(-1, result.indexOf(false));
+    });
+
+    it('Visafone numbers', function() {
+      let result = prefixes.Visafone.map(function(prefix) {
+        return naijaNumber.isValid(prefix + suffix);
+      });
+      assert.equal(-1, result.indexOf(false));
+    });
+
+    it('Starcomms numbers', function() {
+      let result = prefixes.Starcomms.map(function(prefix) {
+        return naijaNumber.isValid(prefix + suffix);
+      });
+      assert.equal(-1, result.indexOf(false));
+    });
+
+    it('Multilinks numbers', function() {
+      let result = prefixes.Multilinks.map(function(prefix) {
+        return naijaNumber.isValid(prefix + suffix);
+      });
+      assert.equal(-1, result.indexOf(false));
+    });
+
+    it('Zoom numbers', function() {
+      let result = prefixes.Zoom.map(function(prefix) {
+        return naijaNumber.isValid(prefix + suffix);
+      });
+      assert.equal(-1, result.indexOf(false));
+    });
+
+    it('MTEL numbers', function() {
+      let result = prefixes.MTEL.map(function(prefix) {
+        return naijaNumber.isValid(prefix + suffix);
+      });
+      assert.equal(-1, result.indexOf(false));
+    });
+
   });
 
   describe('Length of digits.', function() {
     it('should return true for eleven (11) digit numbers.', function() {
-      assert.equal(true, naijaNumber.isValid('08038154929'));
+      assert.equal(true, naijaNumber.isValid('081028582392'));
     });
 
     it('should return true for twelve (12) digit numbers.', function() {
@@ -24,7 +102,7 @@ describe('Test Numbers', function() {
       assert.equal(false, naijaNumber.isValid('080481447'));
     });
 
-    it('should return false for > 13 digit numbers.', function() {
+    it('should return false for > 12 digit numbers.', function() {
       assert.equal(false, naijaNumber.isValid('0803815492999'));
     });
   });
