26234899824df91a05f6d251891323e76f9c2297
Udo <writeudo@gmail.com>
2017-06-21 08:18:58 +0200

    clean out multiple white spaces before testing and supply useful comments

--- index.js
+++ index.js
@@ -15,12 +15,16 @@ function isValid(n) {
 
   if (typeof n === 'number') {
 
+    // numbers never begin with 0, force this to become a string
     number = '0' + n;
 
   } else if (typeof n === 'string') {
 
     firstChar = n.substring(0, 1);
 
+    // user may supply 0 before the number or not
+    // e.g 0703 or 703 (two types of people ¯\_(ツ)_/¯)
+    // either way supply missing leading 0
     number = (firstChar === '0') ? n : '0' + n;
 
   } else {
@@ -29,6 +33,7 @@ function isValid(n) {
 
   }
 
-  return pattern.test(number);
+  // remove all whitespace(s) before running test 
+  return pattern.test(number.replace(/\s+/g, ''));
 
 };
