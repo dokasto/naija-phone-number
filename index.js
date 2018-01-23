b63f68178b448ba0550ae9c8e5dc1dce291ae5c4
Akinjide Bankole <r@akinjide.me>
2018-01-23 00:52:37 -0800

    feat: migrate to promise-based and callback fn (legacy fallback interface)

--- index.js
+++ index.js
@@ -3,37 +3,51 @@ module.exports = { isValid };
 /**
  * Validate phone number
  * @param  {string}  n
- * @return {Boolean} 
+ * @return {Boolean}
  */
-function isValid(n) {
-
-  var firstChar;
+function isValid(n, callback) {
+  if ('function' != typeof callback) callback = function () {};
   var number;
   var pattern = /^([0]{1})([7-9]{1})([0|1]{1})([\d]{1})([\d]{7,8})$/g;
+  var valid;
 
-  if (!n || n.length < 5) return false;
-
-  if (typeof n === 'number') {
-
+  var promise = new Promise(function(resolve, reject) {
+    if (!n || n.length < 5) reject(false);
     // numbers never begin with 0, force this to become a string
-    number = '0' + n;
-
-  } else if (typeof n === 'string') {
-
-    firstChar = n.substring(0, 1);
-
+    if (isNumber(n)) number = '0' + n;
     // user may supply 0 before the number or not
     // e.g 0703 or 703 (two types of people ¯\_(ツ)_/¯)
     // either way supply missing leading 0
-    number = (firstChar === '0') ? n : '0' + n;
+    if (isString(n)) number = (n.substring(0, 1) === '0') ? n : '0' + n;
+    if (!isNumber(n) && !isString(n)) reject(false);
+    // remove all whitespace(s) before running test
+    resolve(pattern.test(number.replace(/\s+/g, '')));
+  });
 
-  } else {
-
-    return false;
-
-  }
-
-  // remove all whitespace(s) before running test 
-  return pattern.test(number.replace(/\s+/g, ''));
-
+  promise.then(callback.bind(null, null), callback);
+  return promise;
 };
+
+/**
+ * Check for plain number.
+ *
+ * @param {Mixed} val
+ * @return {Boolean}
+ * @api private
+ */
+
+function isNumber(val) {
+  return Number == val.constructor;
+}
+
+/**
+ * Check for plain string.
+ *
+ * @param {Mixed} val
+ * @return {Boolean}
+ * @api private
+ */
+
+function isString(val) {
+  return String == val.constructor;
+}
