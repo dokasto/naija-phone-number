acafaf1192a7353ed9004037c48766edf2debc09
Akinjide Bankole <r@akinjide.me>
2018-02-08 12:23:34 +0100

    feat: add dependencies for type checking

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 module.exports = { isValid };
 
 /**
+ * Module dependencies.
+ */
+
+var is = require('is');
+
+
+/**
  * Validate phone number
  * @param  {string}  n
  * @return {Boolean}
@@ -14,12 +21,12 @@ function isValid(n, callback) {
   var promise = new Promise(function(resolve, reject) {
     if (!n || n.length < 5) reject(false);
     // numbers never begin with 0, force this to become a string
-    if (isNumber(n)) number = '0' + n;
+    if (is.number(n)) number = '0' + n;
     // user may supply 0 before the number or not
     // e.g 0703 or 703 (two types of people ¯\_(ツ)_/¯)
     // either way supply missing leading 0
-    if (isString(n)) number = (n.substring(0, 1) === '0') ? n : '0' + n;
-    if (!isNumber(n) && !isString(n)) reject(false);
+    if (is.string(n)) number = (n.substring(0, 1) === '0') ? n : '0' + n;
+    if (!is.number(n) && !is.string(n)) reject(false);
     // remove all whitespace(s) before running test
     resolve(pattern.test(number.replace(/\s+/g, '')));
   });
@@ -27,27 +34,3 @@ function isValid(n, callback) {
   promise.then(callback.bind(null, null), callback);
   return promise;
 };
-
-/**
- * Check for plain number.
- *
- * @param {Mixed} val
- * @return {Boolean}
- * @api private
- */
-
-function isNumber(val) {
-  return Number == val.constructor;
-}
-
-/**
- * Check for plain string.
- *
- * @param {Mixed} val
- * @return {Boolean}
- * @api private
- */
-
-function isString(val) {
-  return String == val.constructor;
-}
